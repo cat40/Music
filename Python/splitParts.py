996a5f9f96ffbb76a636cf17a25df3de7a8f4ba0
cat40 <19844790+cat40@users.noreply.github.com>
2018-05-17 13:40:23 -0400

    inital commit

---
+++ Python/splitParts.py
@@ -0,0 +1,11 @@
+'''
+python script for splitting a lilypond file into parts
+
+Functions to add:
+Split a single file into files of single parts
+
+Split a single file into files of parts with sepcified instruments,
+such that lines outside of the instrument's range have been transposed into the range
+(should optimize so that maximum possible is in range, but NOT partial transposition
+Also consider transposing by other than an octave (5th or 4th)
+'''
